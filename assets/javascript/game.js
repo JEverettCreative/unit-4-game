6e0c98fd9cbdb2b5c7484ce209ac360aa285d5ec
JEverett <jonathan@jonathaneverettcreative.com>
2019-02-14 15:23:31 -0600

    Added game.js pseudocode outline, text for instructions and border for the col, and the gem images to be used were added to the folder.

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+// Game page loads window before executing code
+$(window).load(function(){
+
+    // Set global variables. Set baselines that aren't intended to change match-to-match
+
+    // Initialize game settings for first and resets
+
+    // Generate a random Goal Score between 19-120, assign it to HTML element on page
+
+    // Generate random numbers for each Gem between 1-12
+        // Assign numbers to variables that represent each gem's value
+
+    // Create on click function for each gem
+        // Should add gem's value to User Score
+
+    // Win parameters function
+        // If User Score === Goal Score, then 
+            // Win counter goes up by 1
+            // Add text within Win/Loss box that says "Victory!"
+            // Reset function runs to establish new values
+
+    // Lose parameters function
+        // If User Score > Goal Score, then 
+            // Lose counter goes up by 1
+            // Add text within Win/Loss box that says "You lost!"
+            // Reset function runs to establish new values
+
+
+
+
+
+});
