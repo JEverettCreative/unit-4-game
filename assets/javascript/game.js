fef1b973ac93fa930aac35a4a1529f3402e9343f
JEverett <jonathan@jonathaneverettcreative.com>
2019-02-15 14:59:14 -0600

    Finished bulk of the page layout and started some styles. Game.js file added global variables foreseen, document.ready to wrap all functions within, and created functions to generate the random Goal and the individual gem numbers.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,35 @@
 // Game page loads window before executing code
-$(window).load(function(){
+$(document).ready(function(){
 
     // Set global variables. Set baselines that aren't intended to change match-to-match
+    var goalScore;
+    var emeraldNum;
+    var citrineNum;
+    var rubyNum;
+    var sapphireNum;
+    var winCounter = 0;
+    var lossCounter = 0;
+    var playerScore;
 
     // Initialize game settings for first and resets
+    // initalizeGame = function() {
+    //     $()
+    // }
 
     // Generate a random Goal Score between 19-120, assign it to HTML element on page
-
+    createRandomGoal = function() {
+        goalScore = Math.floor(Math.random() * 103) + 18;
+        $("#goal-score").html(goalScore);
+    }
     // Generate random numbers for each Gem between 1-12
         // Assign numbers to variables that represent each gem's value
+    createGemNumbers = function() {
+        emeraldNum = Math.floor(Math.random() * 13);
+        citrineNum = Math.floor(Math.random() * 13);
+        rubyNum = Math.floor(Math.random() * 13);
+        sapphireNum = Math.floor(Math.random() * 13);
+        
+    }
 
     // Create on click function for each gem
         // Should add gem's value to User Score
@@ -28,5 +49,5 @@ $(window).load(function(){
 
 
 
-
+    createRandomGoal();
 });
