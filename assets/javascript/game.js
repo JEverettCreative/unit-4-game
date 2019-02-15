ec949f3adabbf797e9dc2299f933a56984d22cc9
JEverett <jonathan@jonathaneverettcreative.com>
2019-02-15 15:39:20 -0600

    All basic game functionality has been established. No bugs detected so far. Values reset properly and win/loss parameters work as expected. Next step is to add more style and deploy on Git pages. More game flourishes may be added.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,9 +12,13 @@ $(document).ready(function(){
     var playerScore = 0;
 
     // Initialize game settings for first and resets
-    // initalizeGame = function() {
-    //     $()
-    // }
+    resetGame = function() {
+        createRandomGoal();
+        createGemNumbers();
+        // Set player score back to 0 and then show that on page
+        playerScore = 0;
+        $("#player-score").html(playerScore);
+    }
 
     // Generate a random Goal Score between 19-120, assign it to HTML element on page
     createRandomGoal = function() {
@@ -27,8 +31,7 @@ $(document).ready(function(){
         emeraldNum = Math.ceil(Math.random() * 12);
         citrineNum = Math.ceil(Math.random() * 12);
         rubyNum = Math.ceil(Math.random() * 12);
-        sapphireNum = Math.ceil(Math.random() * 12);  
-        console.log(emeraldNum);    
+        sapphireNum = Math.ceil(Math.random() * 12);     
     }
 
     // Create on click function for each gem
@@ -38,32 +41,46 @@ $(document).ready(function(){
             playerScore += emeraldNum;
             $("#player-score").html(playerScore);
             console.log(playerScore);
+            winLoseCondition();
         });
 
         $("#citrine").on("click", function(){
             playerScore += citrineNum;
             $("#player-score").html(playerScore);
             console.log(playerScore);
+            winLoseCondition();
         });
 
         $("#ruby").on("click", function(){
             playerScore += rubyNum;
             $("#player-score").html(playerScore);
             console.log(playerScore);
+            winLoseCondition();
         });
 
         $("#sapphire").on("click", function(){
             playerScore += sapphireNum;
             $("#player-score").html(playerScore);
             console.log(playerScore);
+            winLoseCondition();
         });
     }
 
     // Win parameters function
-        // If User Score === Goal Score, then 
-            // Win counter goes up by 1
-            // Add text within Win/Loss box that says "Victory!"
-            // Reset function runs to establish new values
+    winLoseCondition = function(){
+        if (playerScore === goalScore) {
+            winCounter += 1;
+            $("#win-count").html("Win: " + winCounter);
+            $("#win-lose").html("Victory!");
+            resetGame();
+        }
+        else if (playerScore > goalScore) {
+            lossCounter += 1;
+            $("#loss-count").html("Loss: " + lossCounter);
+            $("#win-lose").html("Round lost!");
+            resetGame();
+        }
+    }
 
     // Lose parameters function
         // If User Score > Goal Score, then 
